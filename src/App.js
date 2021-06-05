dbe6fd5c14dec2dff8d674dd36ff61ace84c8dea
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-06-04 23:12:59 -0600

    added styled components & google fonts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+  `
 
 function App() {
   return (
+    <>
+    <h1>Mangiamo</h1>
+    <GlobalStyle />
     <div>Hello Mangiamo</div>
+    </>
   );
 }
 
