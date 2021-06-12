69fa890f802b279293aa5f64a4b05115d63149c1
Mia <miamorticia@Mias-MBP.lan>
2021-06-12 16:34:58 -0600

    add banner and navbar fixed position

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -3,11 +3,12 @@ import styled from "styled-components";
 import { pizzaRed } from "../Styles/colors";
 import { Title } from "../Styles/title";
 
-
 const NavbarStyled = styled.div`
     background-color: ${pizzaRed};
     padding: 10px;
-    
+    position: fixed;
+    width: 100%;
+    z-index: 999;
 `
 
 const Logo  = styled(Title)`
@@ -15,7 +16,6 @@ const Logo  = styled(Title)`
     color: white;
     text-shadow: 1px 1px 4px #380502;
 `
-
 
 export function Navbar() {
     return <NavbarStyled>
