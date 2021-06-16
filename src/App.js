47ac806bcfe9465aa2abeeca6b99893aebab4f27
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-06-16 12:20:48 -0600

    push before firebase intergration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import React, { useState } from "react";
 import { Navbar } from "./Navbar/Navbar";
 import { Banner } from "./Banner/Banner";
+import { Menu } from "./Menu/Menu";
+import { FoodDialog } from "./FoodDialog/FoodDialog";
+import { GlobalStyle } from "./Styles/GlobalStyle";
+import { Order } from "./Order/Order";
+import { useOpenFood } from "./Hooks/useOpenFood";
+import { useOrders } from "./Hooks/useOrders";
+import { useTitle } from "./Hooks/useTitle";
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Open Sans', sans-serif;
-  }
-
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-  `
 
 function App() {
+  const openFood = useOpenFood();
+  const orders = useOrders();
+  useTitle({ ...openFood, ...orders });
+
   return (
     <>
-    <GlobalStyle />
-    <Navbar />
-    <Banner />
-    <div>Hello Mangiamo</div>
+      <GlobalStyle />
+      <FoodDialog {...openFood} {...orders} />
+      <Navbar />
+      <Order {...orders} {...openFood} />
+      <Banner />
+      <Menu {...openFood} />
     </>
   );
 }
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,24 +4,28 @@ import { pizzaRed } from "../Styles/colors";
 import { Title } from "../Styles/title";
 
 const NavbarStyled = styled.div`
-    background-color: ${pizzaRed};
-    padding: 10px;
-    position: fixed;
-    width: 100%;
-    z-index: 999;
-`
+  background-color: ${pizzaRed};
+  padding: 10px;
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+`;
 
-const Logo  = styled(Title)`
-    font-size: 20px;
-    color: white;
-    text-shadow: 1px 1px 4px #380502;
-`
+const Logo = styled(Title)`
+  font-size: 20px;
+  color: white;
+  text-shadow: 1px 1px 4px #380502;
+`;
 
 export function Navbar() {
-    return <NavbarStyled>
-        <Logo>
-            Mangiamo 🍕
-        </Logo>
-        </NavbarStyled>
-    
+  return (
+    <NavbarStyled>
+      <Logo>
+        Sliceline{" "}
+        <span role="img" aria-label="pizza slice">
+          🍕
+        </span>
+      </Logo>
+    </NavbarStyled>
+  );
 }
